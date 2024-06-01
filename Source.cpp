07cfc3685d72c32fa4a6cb5be3dcc63b0574bb9a
PaulBelov0 <153445912+PaulBelov0@users.noreply.github.com>
2024-06-01 13:02:09 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+﻿// Transport_List.cpp: определяет точку входа для приложения.
+//
+
+#include "Source.h"
+
+int main()
+{
+
+	return 0;
+}
---
+++ hash_map/Map.cpp
@@ -0,0 +1,23 @@
+//Hash_Map
+
+template <typename T>
+class Map
+{
+public:
+	Map();
+	~Map();
+private:
+
+};
+
+template <typename T>
+Map<T>::Map()
+{
+
+}
+
+template <typename T>
+Map<T>::~Map()
+{
+
+}
