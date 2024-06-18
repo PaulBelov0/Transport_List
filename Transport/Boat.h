a930a7122718f21816582546b3486edbc6a2079b
PaulBelov0 <153445912+PaulBelov0@users.noreply.github.com>
2024-06-18 16:33:42 +0300

    Подготовка к переносу в мастер

---
+++ Transport/Boat.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#ifndef _BOAT_H_
+#define _BOAT_H_
+
+#include <Transport/TransportBase/TransportBase.h>
+
+class Boat : public TransportBase
+{
+public:
+	Boat(uint32_t ID);
+	~Boat();
+
+	inline void editDisplacement(int displacement);
+	inline void editScrewDepth(int screwDepth);
+	inline void print() override;
+	inline std::string finderOnValueType(char typeOfValue) override;
+
+private:
+	int displacement;
+	int screwDepth;
+};
+
+#endif
