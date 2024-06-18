ff0038e633615d05c706ff3c5ebfd8de86ef39b6
PaulBelov0 <153445912+PaulBelov0@users.noreply.github.com>
2024-06-18 16:33:42 +0300

    Подготовка к переносу в мастер

---
+++ Transport/Car.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef _CAR_H_
+#define _CAR_H_
+
+#include <Transport/TransportBase/TransportBase.h>
+
+class Car : public TransportBase
+{
+public:
+	Car(uint32_t ID);
+	~Car();
+
+	inline void editOwnersQuantity(int ownersQuantity);
+	inline void editMileage(int mileage);
+	inline void print() override;
+	inline std::string finderOnValueType(char typeOfValue) override;
+
+private:
+
+	int ownersQuantity;
+	int mileage;
+};
+
+#endif
